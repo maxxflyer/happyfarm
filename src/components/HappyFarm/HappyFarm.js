e31725f6daa9e9f5f136027fcc619286f1f4a4e4
maxxflyer <maxxflyer@gmail.com>
2020-09-05 13:55:31 +0200

    Create a React app and publish it to GitHub Pages

--- src/components/HappyFarm/HappyFarm.js
+++ src/components/HappyFarm/HappyFarm.js
@@ -7,7 +7,7 @@ class HappyFarm extends Component {
     render(){
         return ( 
         <div id="flyer"><div id="HappyFarm_mainlabel">HappyFarm</div>
-            <img src="https://i.imgur.com/BmHURGw.png"/>
+            <img src="https://i.imgur.com/Hs3uInv.png"/>
             <div id="HappyFarm_sublabel">Farming Lambos since 2020</div>
         </div>
         );
