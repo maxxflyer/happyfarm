fa91ef2c20a0a7866b7ecf7f184cc458cf07c4bd
maxxflyer <maxxflyer@gmail.com>
2020-09-05 13:10:04 +0200

    Create a React app and publish it to GitHub Pages

--- src/components/Wing/Wing.js
+++ src/components/Wing/Wing.js
@@ -1,34 +1,55 @@
 import React, { Component } from 'react'
+import Web3 from 'web3';
 import './css.css';
 class Wing extends Component {
+
+  componentDidMount() {
+    this.loadBlockchainData()
+}
+
+
+    async loadBlockchainData() {
+      const beans = new web3.eth.Contract(ABI, ADDRESS) 
+      let supply = await beans.methods.balanceOf("0x510f0a380c914928386bdA31dC159FcB30Ffa708").call()
+      console.log("SUPPLY      "+supply)
+      document.getElementById("beans_counter_label").innerHTML=supply/1000000000000000000 
+    }
+
+    async enable(){
+      window.ethereum.enable().then(async function (accounts) {
+         console.log(accounts[0]);
+         const beans = new web3.eth.Contract(ABI, ADDRESS) 
+      let supply = await beans.methods.balanceOf(accounts[0]).call()
+      console.log("SUPPLY      "+supply)
+      document.getElementById("beans_counter_label").innerHTML=supply/1000000000000000000
+         document.getElementById('metamask').style.display="none";
+         document.getElementById('lips_account').style.display="block";
+         let res = accounts[0].substring(0, 6)+"..."+accounts[0].substring(38);
+         document.getElementById("lips_account_address").innerHTML = res;
+         document.getElementById("lips_account_address").setAttribute("jump", accounts[0])
+      });
+    }
+    
+
     render(){
         return (<div id="login">
-          <div id="lips_search_panel"><img id="lips_search_icon" src="./Beans.png" onClick={search}/></div>
-            <div id="metamask"><img id="metamask_logo" src="http://hivemuse.com/lips/2/metamask.png" alt="metamask" onClick={enable}/></div>
+          <div id="lips_search_panel"><div id="beans_counter_label">0.0</div><div id="beans_counter_image"><img id="lips_search_icon" src="https://i.imgur.com/Hs3uInv.png"/></div></div>
+            <div id="metamask"><img id="metamask_logo" src="http://hivemuse.com/lips/2/metamask.png" alt="metamask" onClick={this.enable}/></div>
             <div className="lips_menu" id="lips_account"  onClick={jump}><div id="lips_account_address"></div></div>
             
         </div>);
     }
 }
 
-function enable(){
-    window.ethereum.enable().then(function (accounts) {
-       console.log(accounts[0]);
-       document.getElementById('metamask').style.display="none";
-       document.getElementById('lips_account').style.display="block";
-       let res = accounts[0].substring(0, 6)+"..."+accounts[0].substring(38);
-       document.getElementById("lips_account_address").innerHTML = res;
-       document.getElementById("lips_account_address").setAttribute("jump", accounts[0])
-    });
-  }
+        let ABI=[{"inputs":[{"internalType":"address","name":"proxy","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"approved","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"operator","type":"address"},{"indexed":false,"internalType":"bool","name":"approved","type":"bool"}],"name":"ApprovalForAll","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[{"internalType":"address","name":"operator","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"approve","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"subject","type":"address"},{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"bytes","name":"_data","type":"bytes"}],"name":"checkOnERC721Received","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"getApproved","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getProxy","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"operator","type":"address"}],"name":"isApprovedForAll","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ownerOf","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"subject","type":"address"},{"internalType":"address","name":"operator","type":"address"},{"internalType":"bool","name":"forAll","type":"bool"},{"internalType":"bool","name":"approved","type":"bool"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"raiseApprovalEvent","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"raiseTransferEvent","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"bytes","name":"data","type":"bytes"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"operator","type":"address"},{"internalType":"bool","name":"_approved","type":"bool"}],"name":"setApprovalForAll","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"proxy","type":"address"}],"name":"setProxy","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes4","name":"interfaceId","type":"bytes4"}],"name":"supportsInterface","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"index","type":"uint256"}],"name":"tokenByIndex","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"uint256","name":"index","type":"uint256"}],"name":"tokenOfOwnerByIndex","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"tokenURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"transferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"}]
+        let ADDRESS="0x801F90f81786dC72B4b9d51Ab613fbe99e5E4cCD"
+        const web3 = new Web3(Web3.givenProvider || "http://localhost:8545")
 
   function jump(){
-    window.open("?addr="+document.getElementById("lips_account_address").getAttribute("jump"))
+    window.open("https://etherscan.io/address/"+document.getElementById("lips_account_address").getAttribute("jump"))
   }
 
-  function search(){
-    window.open("?addr="+document.getElementById("lips_search_field").value)
-  }
+
 
 
 export default Wing
