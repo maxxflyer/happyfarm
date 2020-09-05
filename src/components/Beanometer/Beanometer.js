d7760b4d61b4e4f5c0045896600d3b2816b6f4f7
maxxflyer <maxxflyer@gmail.com>
2020-09-05 16:37:14 +0200

    Create a React app and publish it to GitHub Pages

--- src/components/Beanometer/Beanometer.js
+++ src/components/Beanometer/Beanometer.js
@@ -9,6 +9,7 @@ class Beanometer extends Component {
     render(){
         return ( 
         <div id="flyer"><div id="HappyFarm_mainlabel">Beanometer</div>
+        <img id="beanstalk" src="https://i.imgur.com/n1NsRd8.png"/>
          <input  id="gofarm_button" type="button" value="GO FARMING!" onClick={() => this.props.update("HappyFarm")}/>
         </div>
         );
