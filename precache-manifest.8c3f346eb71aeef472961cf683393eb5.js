e100546fe26db0ff5fec00b3cf9120ef292bf1e5
maxxflyer <maxxflyer@gmail.com>
2020-09-05 09:14:50 +0200

    Updates

---
+++ precache-manifest.8c3f346eb71aeef472961cf683393eb5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0da010d306aae9e585e4ba4f24bd37d0",
+    "url": "/happyfarm/index.html"
+  },
+  {
+    "revision": "4a788be3ac5f3c4287ed",
+    "url": "/happyfarm/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e869f7f2c1413f86ed76",
+    "url": "/happyfarm/static/js/2.b3a424df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/happyfarm/static/js/2.b3a424df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a788be3ac5f3c4287ed",
+    "url": "/happyfarm/static/js/main.ee83deeb.chunk.js"
+  },
+  {
+    "revision": "be03aa12d03bc93d3ebc",
+    "url": "/happyfarm/static/js/runtime-main.0896056b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/happyfarm/static/media/logo.5d5d9eef.svg"
+  }
+]);
