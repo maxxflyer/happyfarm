87ccaddb3a2751faf75efc08302ca25a406e6480
maxxflyer <maxxflyer@gmail.com>
2020-09-07 17:13:48 +0200

    update

--- src/components/HappyFarm/HappyFarm.js
+++ src/components/HappyFarm/HappyFarm.js
@@ -68,7 +68,10 @@ class HappyFarm extends Component {
                     <div class="descr">4) Select Menu FARMING</div>
                     <div class="descr">5) Choose your favorite pair and add it in the suggested proportions (Stake)</div>
                     <div class="descr">6) You are ready to farm Beans🌱</div>
-                    <div class="descr_min"><i>more pairs/offers may be added later</i></div>
+                    <div class="descr_min">🌷<i>more pairs/offers may be added later</i></div>
+                    <br></br>
+                    <div class="descr_min">HappyFarm! Farming together the Farm of the future!</div>
+
                     </div>
                     <div class="descr_container_dark">
                     <div class="mini_descr">Beans🌱 are equities of HappyFarm DFO</div>
