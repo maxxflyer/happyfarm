b0b4c2fbfd494437b8a31335d4fcc098cad1479e
maxxflyer <maxxflyer@gmail.com>
2020-09-07 18:19:50 +0200

    update

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -15,6 +15,7 @@ class Logo extends Component {
                 <div className="lips_menu_link lips_dir_button float_left" href="https://dapp.dfohub.com/"   onClick={() => this.props.update("HappyFarm")}>FARM<br></br><img className="lips_dir_icon" src="https://i.imgur.com/dcCX1vC.png"/></div>
                 <div><a className="lips_menu_link lips_dir_button float_right" href="https://docs.google.com/document/d/1r-wu8k9opLyIg3FW6HEFQ9aYl5X6oFi8-VZsTjHR0tA/edit?usp=sharing" target="_blank">PAPER<br></br><img className="lips_dir_icon" src="https://i.imgur.com/pOEwIJ5.png"/></a></div>
                 <div><a className="lips_menu_link lips_dir_button float_right" href="https://github.com/maxxflyer/happyfarm/blob/master/README.md" target="_blank">GITHUB<br></br><img className="lips_dir_icon" src="https://i.imgur.com/Roc9b0U.png"/></a></div>
+                <div><a className="lips_menu_link lips_dir_button float_right" href="https://t.me/joinchat/F1MMkRlZ2sosNU5gcz32-g" target="_blank">TELEGRAM<br></br><img className="lips_dir_icon" src="https://i.imgur.com/6Nkdu3P.png"/></a></div>
                 <div><a className="lips_menu_link lips_dir_button float_right" href="https://twitter.com/maxxflyer" target="_blank">TWITTER<br></br><img className="lips_dir_icon" src="https://i.imgur.com/WCPB544.png"/></a></div>
                   <div><a className="lips_menu_link lips_dir_button float_right" href="https://discord.gg/F3Cqksx" target="_blank">DISCORD<br></br><img className="lips_dir_icon" src="https://i.imgur.com/tZbrqGS.png"/></a></div>
                 </div>
