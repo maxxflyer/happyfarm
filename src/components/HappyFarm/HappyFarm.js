6f9549ea8931e8b62eab5db233b70e359e2a3bb3
maxxflyer <maxxflyer@gmail.com>
2020-09-07 13:53:57 +0200

    update

--- src/components/HappyFarm/HappyFarm.js
+++ src/components/HappyFarm/HappyFarm.js
@@ -59,18 +59,7 @@ class HappyFarm extends Component {
                             <img class="offer_icon" src="https://i.imgur.com/FPL7c5a.png"/>
                         </div>
                     </div>
-                    <div id="farm_offer">
-                        <div id="farm_offer_icons">
-                        <img class="farm_icon" src="https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x9E78b8274e1D6a76a0dBbf90418894DF27cBCEb5/logo.png"/>
-                        </div>
-                        <div id="farm_offer_farm">
-                        UniFi
-                        </div>
-                        <div id="farm_offer_logic">
-                            <div class="reward"><img class="offer_icon" src="https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x9E78b8274e1D6a76a0dBbf90418894DF27cBCEb5/logo.png"/><div class="math">=</div><img class="offer_icon" src="https://www.unifihub.com/assets/img/m4.png"/><div class="math">+</div></div>
-                            <img class="offer_icon" src="https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x9E78b8274e1D6a76a0dBbf90418894DF27cBCEb5/logo.png"/>
-                        </div>
-                    </div><br></br>
+                    <br></br>
                     <center>UNDER CONSTRUCTION!</center>
                 </div>
         </div>
