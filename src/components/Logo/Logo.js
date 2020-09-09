9b9ee7399615ae01401c18fade76ecc1e65b428e
maxxflyer <maxxflyer@gmail.com>
2020-09-09 12:09:40 +0200

    update

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -11,6 +11,7 @@ class Logo extends Component {
             <div id="lips_menu">
                 <div className="menu_row">
                 <div className="lips_menu_link lips_dir_button float_left" href="https://dapp.dfohub.com/"    onClick={() => this.props.update("DFO")}>DFO<br></br><img className="lips_dir_icon" src="https://i.imgur.com/XYKQ1X5.png"/></div>
+                <div className="lips_menu_link lips_dir_button float_left" href="https://dapp.dfohub.com/"    onClick={() => this.props.update("Beanometer")}>ACCOUNT<br></br><img className="lips_dir_icon" src="https://i.imgur.com/mEEm4rN.png"/></div>
                 <div className="lips_menu_link lips_dir_button float_left" href="https://uniswap.info/token/0x801f90f81786dc72b4b9d51ab613fbe99e5e4ccd"   onClick={() => this.props.update("Beans")}>TOKEN<br></br><img className="lips_dir_icon" src="https://i.imgur.com/dcCX1vC.png"/></div>
                 <div className="lips_menu_link lips_dir_button float_left" href="https://dapp.dfohub.com/"   onClick={() => this.props.update("HappyFarm")}>FARM<br></br><img className="lips_dir_icon" src="https://i.imgur.com/YAqCUoc.png"/></div>
                 <div><a className="lips_menu_link lips_dir_button float_right" href="https://docs.google.com/document/d/1r-wu8k9opLyIg3FW6HEFQ9aYl5X6oFi8-VZsTjHR0tA/edit?usp=sharing" target="_blank">PAPER<br></br><img className="lips_dir_icon" src="https://i.imgur.com/pOEwIJ5.png"/></a></div>
